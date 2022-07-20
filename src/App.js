4a7b3063709674c2c88bcc32d2ab2884c65b2843
sinsin <wr1126@naver.com>
2022-07-20 16:49:00 +0900

    create converter with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function MinutesToHours() {
+  const [time, setTime] = useState(0);
+  const [mToh, setMtoH] = useState(false); //minutes to hours
+
+  function onChange(e) {
+    setTime(e.target.value);
+  }
+  function reset() {
+    setTime(0);
+  }
+  function onFlip() {
+    reset();
+    setMtoH((current) => !current);
+  }
+
+  return (
+    <>
+      <h3>{mToh ? "Hours to Minutes" : "Minutes to Hours"}</h3>
+      <div>
+        <label htmlFor="minutes">Minutes </label>
+        <input
+          value={mToh ? time * 60 : time}
+          onChange={onChange}
+          id="minutes"
+          placeholder="minutes"
+          type="number"
+          disabled={mToh ? true : false}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours </label>
+        <input
+          value={mToh ? time : time / 60}
+          onChange={onChange}
+          id="hours"
+          placeholder="hours"
+          type="number"
+          disabled={mToh ? false : true}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>
+        {mToh ? "Minutes to Hours" : "Hours to Minutes"}
+      </button>
+    </>
+  );
+}
+function KmToMiles() {
+  return (
+    <>
+      <h3>Km to Miles</h3>
+    </>
+  );
+}
 
 function App() {
+  const [index, setIndex] = useState("-1");
+
+  function onSelect(e) {
+    setIndex(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="-1">select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
+    </>
   );
 }
 
